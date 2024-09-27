82c9f9f2b8224eae6321d95bfb9fed0e6b32e55a
ashen-crow <k.apifer.dev@gmail.com>
2024-09-27 22:59:04 +0100

    Working plus and minus

--- Calculator/MathCalculator.cs
+++ Calculator/MathCalculator.cs
@@ -8,6 +8,11 @@ namespace Calculator;
 
 public static class MathCalculator
 {
+    public static readonly string numberSubPattern = @"([0-9]+\.)*[0-9]+";
+    public static readonly string escapedPlusSign = @"\+";
+    public static readonly string escapedMinusSign = @"\-";
+    public static readonly string allOperatorsEscaped = @"[\+\-\*\/%]";
+
     public static string FindFirstDeepestInstanceOfUnaryMathFunction(string input)
     {
         // 0. Remove casing issues
@@ -23,6 +28,7 @@ public static class MathCalculator
     public static string ExtractFirstInstanceOfNumberPlusNumber(string input)
     {
         input = input.ToUpper();
+
         Regex pattern = new(@"-{0,1}[0-9]{1,3}\+{1}-{0,1}[0-9]{1,3}");
         throw new NotImplementedException();
     }
@@ -37,14 +43,34 @@ public static class MathCalculator
         return input;
     }
 
-    private static string CalculateSubtractions(string input)
+    public static string CalculateSubtractions(string input)
     {
-        throw new NotImplementedException();
+        while (IsMatchOfNumberMinusNumber(input))
+        {
+            input = CalculateFirstInstanceOfNumberMinusNumber(input);
+            Console.WriteLine(input);
+        }
+        return input;
     }
 
-    private static string CalculateAdditions(string input)
+    public static bool IsMatchOfNumberPlusNumber(string input)
     {
-        throw new NotImplementedException();
+        return new Regex($@"{numberSubPattern}{escapedPlusSign}{numberSubPattern}").IsMatch(input);
+    }
+
+    public static bool IsMatchOfNumberMinusNumber(string input)
+    {
+        return new Regex($@"{numberSubPattern}{escapedMinusSign}{numberSubPattern}").IsMatch(input);
+    }
+
+    public static string CalculateAdditions(string input)
+    {
+        while (IsMatchOfNumberPlusNumber(input))
+        {
+            input = CalculateFirstInstanceOfNumberPlusNumber(input);
+            Console.WriteLine(input);
+        }
+        return input;
     }
 
     private static string CalculateDivisions(string input)
@@ -62,26 +88,30 @@ public static class MathCalculator
         throw new NotImplementedException();
     }
 
-    public static string PLAN(string input)
+    public static string CalculateFirstInstanceOfNumberPlusNumber(string input)
     {
         input = input.ToUpper();
         input = input.Replace(" ", "");
-        // A number looks like 324,203.04 or 24,324,203.04
-        string allOperatorsEscaped = @"[\+\-\*\/%]";
-        string escapedPlusSign = @"\+";
-        string numberSubPattern = @"([0-9]+\.)*[0-9]+";
-
-        // BIDMAS
-
-        // 1. Find deepest instance of number + number:
         var firstMatchOfNumberPlusNumber = new Regex($@"{numberSubPattern}{escapedPlusSign}{numberSubPattern}").Match(input);
         Console.WriteLine("Found first match of number + number: " + firstMatchOfNumberPlusNumber.Value);
-        // Grab the numbers:
         string[] numbers = firstMatchOfNumberPlusNumber.Value.Split('+');
         double number1 = double.Parse(numbers[0]);
         double number2 = double.Parse(numbers[1]);
-        var sum = number1 + number2;
+        double sum = number1 + number2;
+        input = input.Replace(firstMatchOfNumberPlusNumber.Value, sum.ToString());
+        return input;
+    }
 
+    public static string CalculateFirstInstanceOfNumberMinusNumber(string input)
+    {
+        input = input.ToUpper();
+        input = input.Replace(" ", "");
+        var firstMatchOfNumberPlusNumber = new Regex($@"{numberSubPattern}{escapedMinusSign}{numberSubPattern}").Match(input);
+        Console.WriteLine("Found first match of number - number: " + firstMatchOfNumberPlusNumber.Value);
+        string[] numbers = firstMatchOfNumberPlusNumber.Value.Split('-');
+        double number1 = double.Parse(numbers[0]);
+        double number2 = double.Parse(numbers[1]);
+        double sum = number1 - number2;
         input = input.Replace(firstMatchOfNumberPlusNumber.Value, sum.ToString());
         return input;
     }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -11,7 +11,42 @@ public class UnitTest1 // TODO: rename this class to something more meaningful
     public void AddTwoNumbers(string input, string expected)
     {
         // Act
-        string actual = MathCalculator.PLAN(input);
+        string actual = MathCalculator.CalculateFirstInstanceOfNumberPlusNumber(input);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [DataRow("3+4", true)]
+    public void IsMatchOfNumberPlusNumber(string input, bool expected)
+    {
+        // Act
+        bool actual = MathCalculator.IsMatchOfNumberPlusNumber(input);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [DataRow("3+4+5+2", "14")]
+    public void ProcessBidmasExceptBrackets(string input, string expected)
+    {
+        // Act
+        string actual = MathCalculator.CalculateAdditions(input);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [DataRow("3-4", "-1")]
+    [DataRow("20-10", "10")]
+    [DataRow("200-77.5", "122.5")]
+    public void SubtractTwoNumbers(string input, string expected)
+    {
+        // Act
+        string actual = MathCalculator.CalculateSubtractions(input);
 
         // Assert
         Assert.AreEqual(expected, actual);
