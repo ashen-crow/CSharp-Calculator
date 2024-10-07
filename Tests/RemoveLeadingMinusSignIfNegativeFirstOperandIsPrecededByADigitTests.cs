3228302e320429904d4fc6e7aa2a4cea2558c0ba
ashen-crow <k.apifer.dev@gmail.com>
2024-10-07 01:20:33 +0100

    Renamed a func

--- Tests/RemoveLeadingMinusSignIfNegativeFirstOperandIsPrecededByADigitTests.cs
+++ Tests/RemoveLeadingMinusSignIfNegativeFirstOperandIsPrecededByADigitTests.cs
@@ -2,7 +2,7 @@
 class RemoveLeadingMinusSignIfNegativeFirstOperandIsPrecededByADigitTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void RemoveFakeNegatives()
     {
         /* Need to add a function to ensure that negatives are
         real negatives! if the character before a negative
