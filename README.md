a2479500be97d16b35b866a9b861949b5e14fc43
ashen-crow <k.apifer.dev@gmail.com>
2024-10-07 01:20:01 +0100

    template for removing fake negatives

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Remaining work:
 * Negative numbers (all operators should handle negatives of both operators!)
+* Need to add a function to ensure that negatives are real negatives! if the character before a negative first operand is a digit, the first operand is not negative
 * Unary maths functions (more than just the absolute fn)
 * UI
 * API (Function App)
---
+++ Tests/RemoveLeadingMinusSignIfNegativeFirstOperandIsPrecededByADigitTests.cs
@@ -0,0 +1,13 @@
+[TestClass]
+class RemoveLeadingMinusSignIfNegativeFirstOperandIsPrecededByADigitTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        /* Need to add a function to ensure that negatives are
+        real negatives! if the character before a negative
+         first operand is a digit, the first operand is not negative
+        */
+        Assert.Fail();
+    }
+}
